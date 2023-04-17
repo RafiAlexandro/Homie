c986adbf51866f94b5a008ea809afc6ebaff4f70
Rafi Alexandro <halloandro123@gmail.com>
2023-04-17 14:36:11 +0700

    update layout and responsive

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(function () {
+
+    $( ".home-img" ).animate({
+        opacity: 1
+      }, "slow", function() {
+        // Animation complete.
+      });
+
+    $(window).scroll(function () {
+        
+        var scroll = $(window).scrollTop();
+
+        // Navbar
+        if (scroll >= 50) {
+            $("#nav").addClass("bg-navbar");
+        } else {
+            $("#nav").removeClass("bg-navbar");
+        };
+
+        // Feature
+        if (scroll >= 100) {
+
+            $("#featured").animate({
+                opacity: 1
+            }, "slow", function() {
+                // Animation complete.
+            });
+
+        };
+
+        // Modern
+        if (scroll >= 1400) {
+
+            $("#modern").animate({
+                opacity: 1
+              }, "slow", function() {
+                // Animation complete.
+            });
+
+        };
+
+    });
+
+
+});
